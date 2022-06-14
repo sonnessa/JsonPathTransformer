e1e672cc61038c551533463cb10819230b9cec81
Michele Sonnessa <michele.sonnessa.visualitics@skytv.it>
2022-06-14 12:04:26 +0200

    Fix behavior introduced in 2.5.7

--- json-path/src/main/java/com/jayway/jsontransformer/spi/transformer/jsonpathtransformer/JsonPathTransformationProvider.java
+++ json-path/src/main/java/com/jayway/jsontransformer/spi/transformer/jsonpathtransformer/JsonPathTransformationProvider.java
@@ -267,7 +267,7 @@ public class JsonPathTransformationProvider implements TransformationProvider<Js
         if (!srcValueIsConstant && srcPath != null) {
             try {
                 srcValue = jsonContext.read(compiledSrcPath);
-                if (srcValue == null && configuration.getOptions().contains(Option.FORCE_MISSING_PROPERTIES_ON_TARGET)) {
+                if (srcValue == null && ! configuration.getOptions().contains(Option.FORCE_MISSING_PROPERTIES_ON_TARGET)) {
                     // TODO: log here. we are going to ignore any additionalTransform as well.
                     return transformed;
                 }
